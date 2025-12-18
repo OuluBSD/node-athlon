7e1f857f580ddee98f5ba443550d2a5cb8761f3a
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-12-18 02:03:16 +0000

    Fix f-string in input.py for Python 3.4.3 compatibility
    - Replace f-string with .format() method in GypError raise statement
    - This addresses another Python 3.6+ syntax that was breaking Python 3.4.3 compatibility
    - Fixed in both tools/gyp and deps/npm/node_modules/node-gyp locations

--- deps/npm/node_modules/node-gyp/gyp/pylib/gyp/input.py
+++ deps/npm/node_modules/node-gyp/gyp/pylib/gyp/input.py
@@ -227,7 +227,7 @@ def LoadOneBuildFile(build_file_path, data, aux_data, includes, is_target, check
     if os.path.exists(build_file_path):
         build_file_contents = open(build_file_path, encoding="utf-8").read()
     else:
-        raise GypError(f"{build_file_path} not found (cwd: {os.getcwd()})")
+        raise GypError("{} not found (cwd: {})".format(build_file_path, os.getcwd()))
 
     build_file_data = None
     try:
